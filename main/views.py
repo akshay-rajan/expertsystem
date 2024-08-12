86fecabacc5071e920b3efde0e1b948978c21581
Akshay Rajan <akshayrajan345@gmail.com>
2024-08-12 21:51:53 +0530

    Add Model Evaluation

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from sklearn.datasets import fetch_california_housing
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from django.http import JsonResponse
 import numpy as np
 import pandas as pd
@@ -24,10 +25,14 @@ def clustering(request):
 # ? Implement Algorithms
 
 def linear_regression(request):
-    """Enable user to input data and get the results of linear regression."""
-    
+    """
+    Enable user to input training and testing sets
+    Build a Linear Regression model
+    Display the results
+    """    
     # On submission of the datasets
     if request.method == 'POST':
+        # ! Data Processing
         # train_set = request.POST.get('train_set')
         # test_set = request.POST.get('test_set')
         
@@ -41,9 +46,7 @@ def linear_regression(request):
         }
         df = pd.DataFrame(data)
         print(df.head())
-        
-        # Preprocessing
-        
+                
         # Features, Target
         # X, y = df['Size'], df['Price']
         X = np.array(df['Size']).reshape(-1, 1)
@@ -52,17 +55,29 @@ def linear_regression(request):
         # Split data into training and testing sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
-        # Train the model
+        # ! Model Building
         model = LinearRegression()
         model.fit(X_train, y_train)
         
-        # Make predictions on the test data
-        # y_pred = model.predict(X_test)
-
-        # Display the predictions
-        # print("Predicted values:", y_pred)
+        # ! Model Evaluation
+        y_pred = model.predict(X_test)
         
-        return render(request, 'main/results.html')
+        mse = mean_squared_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+        
+
+        return render(request, 'main/results.html', {
+            'actual': y_test,
+            'predicted': y_pred,
+            'metrics': {
+                'mse': mse,
+                'rmse': rmse,
+                'mae': mae,
+                'r2': r2,
+            },
+        })
         
     
     # Render the Input Form
