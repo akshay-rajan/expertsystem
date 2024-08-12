c14368e0c0d9c62e96f6d643e62b0ff579b7d747
Akshay Rajan <akshayrajan345@gmail.com>
2024-08-12 22:49:55 +0530

    Fix Bug

--- main/views.py
+++ main/views.py
@@ -93,50 +93,7 @@ def linear_regression(request):
         'test_set': test_set,
     })
 
-def todo(request):
-    """
-    Render the home page. 
-    Let user select a dataset.
-    Redirect to the page for selection of variables.
-    """
-
-    datasets = {
-        'fetch_california_housing': fetch_california_housing,   
-    }
-
-    if request.method == 'POST':
-        dataset_name = request.POST.get('dataset')
-        indep_vars = request.POST.getlist('independent_vars')
-        dep_var = request.POST.get('dependent_var')
-        
-        # Load dataset
-        dataset_func = datasets[dataset_name]
-        dataset = dataset_func()
-        df = pd.DataFrame(data=dataset.data, columns=dataset.feature_names)
-        df['target'] = dataset.target
-
-        X = df[indep_vars]
-        y = df[dep_var]
-
-        # Split the dataset
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-        # Train the model
-        lr = LinearRegression()
-        lr.fit(X_train, y_train)
-
-        # Predict the test values
-        predictions = lr.predict(X_test)
-
-        # Prepare the results
-        results = {
-            'actual': y_test.tolist(),
-            'predicted': predictions.tolist()
-        }
-
-        return JsonResponse(results)
-    else:
-        return render(request, 'main/index.html', {'datasets': datasets.keys()})
+# ? Helper Functions
 
 def get_variables(request):
     dataset_name = request.GET.get('dataset')
