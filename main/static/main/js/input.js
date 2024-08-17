62cd158b241f9062a369d88a844db191febde481
Akshay Rajan <akshayrajan345@gmail.com>
2024-08-17 21:41:21 +0530

    Add demo heatmap

--- main/static/main/js/input.js
+++ main/static/main/js/input.js
@@ -1,3 +1,4 @@
+console.log('input.js loaded');
 function handleFileChange(event) {
   const file = event.target.files[0];
   if (file) $('#upload-btn').prop('disabled', false);
@@ -110,33 +111,117 @@ function validateForm() {
 }
 
 function plot() {
-  return;
-  $("#canvas-1").removeClass("d-none");
-  const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-  const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+  // Remove any existing SVG elements
+  d3.select("#canvas-1").selectAll("*").remove();
 
-  new Chart("myChart", {
-    type: "line",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor:"rgba(0,0,255,1.0)",
-        borderColor: "rgba(0,0,255,0.1)",
-        data: yValues
-      }]
-    },
-    options: {
-      legend: {display: false},
-      scales: {
-        yAxes: [
-          {ticks: 
-            {
-              min: 6, 
-              max:16,
-            }
-          }
-        ],
-      }
-    }
+  // Set the dimensions and margins of the graph
+  var margin = {top: 80, right: 25, bottom: 30, left: 40},
+      width = 450 - margin.left - margin.right,
+      height = 450 - margin.top - margin.bottom;
+
+  // Append the svg object to the body of the page
+  var svg = d3.select("#canvas-1")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Read the data
+  d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
+
+    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+    var myGroups = d3.map(data, function(d){return d.group;}).keys();
+    var myVars = d3.map(data, function(d){return d.variable;}).keys();
+
+    // Build X scales and axis
+    var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(myGroups)
+      .padding(0.05);
+    svg.append("g")
+      .style("font-size", 15)
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickSize(0))
+      .select(".domain").remove();
+
+    // Build Y scales and axis
+    var y = d3.scaleBand()
+      .range([ height, 0 ])
+      .domain(myVars)
+      .padding(0.05);
+    svg.append("g")
+      .style("font-size", 15)
+      .call(d3.axisLeft(y).tickSize(0))
+      .select(".domain").remove();
+
+    // Build color scale
+    var myColor = d3.scaleSequential()
+      .interpolator(d3.interpolateInferno)
+      .domain([1,100]);
+
+    // Create a tooltip
+    var tooltip = d3.select("#canvas-1")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+    // Three functions that change the tooltip when user hovers / moves / leaves a cell
+    var mouseover = function(d) {
+      tooltip.style("opacity", 1);
+      d3.select(this).style("stroke", "black").style("opacity", 1);
+    };
+    var mousemove = function(d) {
+      tooltip
+        .html("The exact value of<br>this cell is: " + d.value)
+        .style("left", (d3.mouse(this)[0]+70) + "px")
+        .style("top", (d3.mouse(this)[1]) + "px");
+    };
+    var mouseleave = function(d) {
+      tooltip.style("opacity", 0);
+      d3.select(this).style("stroke", "none").style("opacity", 0.8);
+    };
+
+    // Add the squares
+    svg.selectAll()
+      .data(data, function(d) {return d.group+':'+d.variable;})
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return x(d.group); })
+        .attr("y", function(d) { return y(d.variable); })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth())
+        .attr("height", y.bandwidth())
+        .style("fill", function(d) { return myColor(d.value); })
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
+
+    // Add title to graph
+    svg.append("text")
+      .attr("x", 0)
+      .attr("y", -50)
+      .attr("text-anchor", "left")
+      .style("font-size", "22px")
+      .text("A d3.js heatmap");
+
+    // Add subtitle to graph
+    svg.append("text")
+      .attr("x", 0)
+      .attr("y", -20)
+      .attr("text-anchor", "left")
+      .style("font-size", "14px")
+      .style("fill", "grey")
+      .style("max-width", 400)
+      .text("A short description of the take-away message of this chart.");
   });
 }
