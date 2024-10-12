dabfbcf0041ab5a168fd35b4c8011a4738a3940b
Akshay Rajan <akshayrajan345@gmail.com>
2024-10-12 23:03:47 +0530

    Minor changes in import

--- main/views.py
+++ main/views.py
@@ -5,14 +5,12 @@ import numpy as np
 import pandas as pd
 from django.conf import settings
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
 from sklearn.cluster import KMeans
-from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier, export_text, plot_tree
-from sklearn.metrics import classification_report, mean_squared_error, r2_score, mean_absolute_error
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, root_mean_squared_error
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import silhouette_score
 
@@ -72,7 +70,7 @@ def linear_regression(request):
         y_pred_modified = [round(i, 3) for i in y_pred]
         
         mse = mean_squared_error(y_test, y_pred)
-        rmse = np.sqrt(mse)
+        rmse = root_mean_squared_error(y_test, y_pred)
         mae = mean_absolute_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
         
@@ -193,9 +191,6 @@ def decision_tree(request):
         recall = recall_score(y_test, y_pred, average='weighted')
         f1 = f1_score(y_test, y_pred, average='weighted')
         
-        tree_rules = export_text(model, feature_names=features)
-        print(tree_rules)
-        
         model_filename = f"decision_tree_{uuid.uuid4().hex[:6]}.pkl"
         model_path = os.path.join(settings.MEDIA_ROOT, model_filename)
         
@@ -229,6 +224,13 @@ def decision_tree(request):
     
     return render(request, 'main/input.html')
 
+def random_forest(request):
+    """Random Forest Classifier"""
+    
+    if request.method == "POST":
+        return render(request, 'main/random_forest.html')
+    
+    return render(request, 'main/input.html')
     
 def kmeans(request):
     """K-Means Clustering"""
