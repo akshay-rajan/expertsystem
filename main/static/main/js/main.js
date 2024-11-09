375377eac884cff2906685a629f8fdef26cef3b2
Akshay Rajan <akshayrajan345@gmail.com>
2024-11-09 23:46:49 +0530

    Add close view code btn, update styles to use boostrap logos

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -53,11 +53,21 @@ $('#show-code-btn').click(() => {
     html: `${sourceCode}`,
     customClass: {
       popup: 'swal-wide',
-      confirmButton: 'btn btn-primary'
+      confirmButton: 'copy-code-btn btn btn-primary d-flex justify-content-center align-items-center',
+      cancelButton: 'cancel-code-btn btn btn-secondary d-flex justify-content-center align-items-center'
     },
     showCloseButton: true,
     showConfirmButton: true,
-    confirmButtonText: 'Copy', // Set default button text to "Copy Code"
+    showCancelButton: true,
+    confirmButtonText: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
+      </svg>`,
+    cancelButtonText: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+      </svg>`,
     didOpen: () => {
       Prism.highlightAll(); // Highlight syntax
     },
@@ -75,7 +85,7 @@ $('#show-code-btn').click(() => {
       // Show success message after copying
       Swal.fire({
         title: 'Copied!',
-        timer: 800,
+        timer: 700,
         showConfirmButton: false
       });
     }
