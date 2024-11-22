905970dbfe5177b95e345ae83f344cf081edf4a6
Akshay Rajan <akshayrajan345@gmail.com>
2024-11-22 19:57:12 +0530

    Add link to home page

--- learn/urls.py
+++ learn/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='learn'),
     path('introduction/', views.introduction, name='introduction'),
     path('machinelearning/', views.machinelearning, name='machinelearning'),
 ]
