2ca13f2bf2a47331b4d63a58b38bc184ca7cbb8b
Akshay Rajan <akshayrajan345@gmail.com>
2024-11-22 19:42:42 +0530

    Fix blog styles

--- learn/urls.py
+++ learn/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('introduction/', views.introduction, name='introduction'),
+    path('machinelearning/', views.machinelearning, name='machinelearning'),
 ]
--- learn/views.py
+++ learn/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def introduction(request):
     return render(request, 'learn/introduction.html')
+
+def machinelearning(request):
+    return render(request, 'learn/machinelearning.html')
